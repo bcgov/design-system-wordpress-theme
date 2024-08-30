eab11fd5414ec63b78ed5e98ae75b4e17531b2b4
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-08-30 15:38:45 -0700

    added changes as per PR

--- checklist.md
+++ checklist.md
@@ -1,4 +1,4 @@
-Created at 2024-08-30 2:28 pm
+Created at 2024-08-30 3:38 pm
 
 * [yes] Updated version in composer.json
 * [yes] Updated version in style.css or plugin file
--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '3ad7c2801e7d6d142ed7');
+<?php return array('dependencies' => array(), 'version' => '771353179dfee4517880');
