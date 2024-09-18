c9209cb7573dd6ee6a645e4a23418b4dcb0dbf11
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-09-18 14:07:51 -0700

    updated style-overrides dir in function php

--- functions.php
+++ functions.php
@@ -55,5 +55,5 @@ function design_system_include_block_style_variations( $dir_path ) {
 }
 
 // Set the directory path and include block style variations.
-$dir_path = get_template_directory() . '/blocks/core';
+$dir_path = get_template_directory() . '/blocks/core/style-overrides';
 design_system_include_block_style_variations( $dir_path );
