a3403ebeaaea8583547e6358b6909105d84fd994
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-10-10 11:28:18 -0700

    testr

--- functions.php
+++ functions.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * Enqueues the design system CSS stylesheet on the frontend of the WordPress site.
- *
- * @since 1.3.0
- */
+
 function design_system_public_enqueue_global_styles() {
     $version = filemtime( get_template_directory() . '/dist/index.css' );
     wp_enqueue_style( 'design-system-styles', get_template_directory_uri() . '/dist/index.css', array(), $version );
@@ -26,7 +22,11 @@ function design_system_enqueue_global_js_scripts() {
 
 add_action( 'enqueue_block_editor_assets', 'design_system_enqueue_global_js_scripts' );
 
-
+/**
+ * Includes all block style variation PHP files from blocks/core directory recursively.
+ *
+ * @param string $dir_path The path of the directory to include files from.
+ */
 function design_system_include_block_style_variations( $dir_path ) {
     // Define the block style variation files.
     $block_style_variation_files = [ 'navigation' ];
