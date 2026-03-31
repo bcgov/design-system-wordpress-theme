0d8c8bebbf18940e11ce391f68efaa668ca16e04
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-03-31 10:45:40 -0700

    Update theme.json (#142)
    * Update theme.json
    
    * added hook for appearance tools for super admins

--- functions.php
+++ functions.php
@@ -226,6 +226,29 @@ function design_system_combine_parent_child_theme_json( $theme_json ) {
 add_filter( 'wp_theme_json_data_theme', 'design_system_combine_parent_child_theme_json' );
 
 /**
+ * Enable appearance tools for super admins only.
+ *
+ * @param WP_Theme_JSON_Data $theme_json Theme JSON data object.
+ *
+ * @return WP_Theme_JSON_Data
+ */
+function design_system_enable_appearance_tools_for_super_admins( $theme_json ) {
+    if ( ! is_super_admin() ) {
+        return $theme_json;
+    }
+
+    return $theme_json->update_with(
+        array(
+            'version'  => 3,
+            'settings' => array(
+                'appearanceTools' => true,
+            ),
+        )
+    );
+}
+add_filter( 'wp_theme_json_data_theme', 'design_system_enable_appearance_tools_for_super_admins', 20 );
+
+/**
  * Add excerpt support to pages.
  */
 add_post_type_support( 'page', 'excerpt' );
