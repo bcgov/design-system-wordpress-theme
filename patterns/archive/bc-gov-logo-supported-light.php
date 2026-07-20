082426b80aaa6bd0df92553c2bea620cdc1bdd1f
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-07-20 13:25:36 -0700

    Update bc-gov-logo-supported-light.php

--- patterns/archive/bc-gov-logo-supported-light.php
+++ patterns/archive/bc-gov-logo-supported-light.php
@@ -12,7 +12,7 @@
 <div class="wp-block-group" style="padding-right:0;padding-left:0">
     <!-- wp:image {"width":"175px","height":"auto","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
     <figure class="wp-block-image size-full is-resized is-style-default"><img
-            src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/BCID_Supported_H_Solid_rev.png'); ?>"
+            src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/BCID_Supported_H_Solid_rev.png' ); ?>"
             alt="Supported by the Province of British Columbia" style="width:175px;height:auto" /></figure>
     <!-- /wp:image -->
 </div>
